f9517a32aefd92ba733e6f184948b2f0c44c8142
Lucas Martin Carballo <lucascarballo8819@gmail.com>
2024-09-30 01:36:09 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',
+    './Views/**/*.cshtml',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vinotinto: '#8B0000',
+        terracota: '#E2725B',
+        celeste: '#00BFFF',
+        contraste: '#FF6347',  // un tono rojizo
+      }
+    },
+  },
+  plugins: [],
+}
+
