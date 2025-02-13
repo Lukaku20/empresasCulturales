4f304c133a6a2cbd338fa11d9c6f09bffbb207e0
Lucas Martin Carballo <lucascarballo8819@gmail.com>
2025-02-13 19:27:31 -0300

    preparando deploy

--- obj/Debug/net8.0/EmpresasCulturales.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmpresasCulturales.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpresasCulturales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0bd4ba0bffc673ba931dbc65b77b749fb98657c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25dbd6b42532439b72648c97cd9c2a7a8fd59e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpresasCulturales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpresasCulturales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
